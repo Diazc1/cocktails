62042fe5e7cec5c18346cf63c3b5a876909c178d
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-25 16:25:46 -0800

    added a call method with puts

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,20 @@
 class Cocktail::CLI
 
     def call
-        puts "im in the cli"
+        puts "Welcome user"
+        puts "To see available margaritas, enter 'margaritas'"
+        puts "To see more options, enter 'more options'"
+        puts "To exit menu, enter 'exit'"
+
     end
-    
+
+    # def menu 
+    #     input = gets.strip.downcase
+
+    #     if
+    #     else
+    #     end
+
+    # end
+
 end
