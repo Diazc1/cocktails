434cf3144c0fa389ed41941bc5888db31b8962e7
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-25 17:01:53 -0800

    created a method menu to interact with user input from the method call

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,18 +3,36 @@ class Cocktail::CLI
     def call
         puts "Welcome user"
         puts "To see available margaritas, enter 'margaritas'"
-        puts "To see more options, enter 'more options'"
         puts "To exit menu, enter 'exit'"
-
+        menu
     end
 
-    # def menu 
-    #     input = gets.strip.downcase
+    def menu         
+        input = gets.strip
 
-    #     if
-    #     else
-    #     end
+        if input == "margaritas"
+            margaritas_list
+            menu
+        elsif input == "exit"
+            goodbye
+        else
+            invalid_entry
+            menu
+        end
+    end
 
-    # end
+    def margaritas_list
+        puts "1. margarita 1"
+        puts "2. margarita 2"
+        puts "3. margarita 3"
+    end
+
+    def invalid_entry
+        puts "Invalid entry, try again"
+    end
+
+    def goodbye
+        puts "Don't drink and drive! Goodbye."
+    end
 
 end
